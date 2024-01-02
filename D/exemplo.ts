10952704160f296819c675ac274300e7dc763846
Carlos Emannoel <carlosemannoel2019@gmail.com>
2024-01-02 08:31:10 -0300

    D - studies

---
+++ D/exemplo.ts
@@ -0,0 +1,18 @@
+interface Movie {
+   play(): void;
+}
+
+interface AudioControl {
+  increaseVolume(): void;
+}
+
+class Movie  {
+ name: string;
+ category: string;
+}
+
+class DramaCategory  {
+ 
+}
+
+
